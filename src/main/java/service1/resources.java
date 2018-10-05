41d6bf163dcefcaf66224fa2f02877391fc78877
ManpreetRajpal <mrajpal@thoughtworks.com>
2018-10-05 19:02:43 +0530

    src/java

---
+++ src/main/java/service1/resources.java
@@ -0,0 +1,13 @@
+package service1;
+
+public class resources {
+
+    //You can call method from the class name with className.method if that method is defined with static
+    public static String placePostData() {
+
+        String res = "/maps/api/place/add/json";
+        return res;
+    }
+
+
+}
