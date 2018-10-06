b8726455369904bc170ea0c5ede7dca5cb4df752
ManpreetRajpal <mrajpal@thoughtworks.com>
2018-10-06 20:32:06 +0530

    using parameters and groups

--- src/test/java/Demo1Test.java
+++ src/test/java/Demo1Test.java
@@ -8,7 +8,7 @@ public class Demo1Test {
 
     private static Logger log = LogManager.getLogger(Demo1Test.class.getName());
 
-    @Test(description = "Description in Demo1 Test")
+    @Test(description = "Description in Demo1 Test",groups = {"Regression"})
     public void run_demo1_test() {
         log.info(" ------------> printing INFO log4j info of DEMO1 ");
         log.warn(" ------------> printing WARN log4j info of DEMO1 ");
--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -2,6 +2,7 @@
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
@@ -9,10 +10,16 @@ public class DemoTest {
 
     private static Logger log = LogManager.getLogger(DemoTest.class.getName());
 
-    @Test(description = "Description in DemoTest")
+    @Test(description = "Description in DemoTest", groups = {"Regression"})
     public void run_demo_test() {
         log.info(" ------------> printing INFO log4j info of DEMO ");
         log.warn(" ------------> printing WARN log4j info of DEMO ");
-        log.error(" ------------> printing ERROR log4j info of DEMO ");
+        log.error(" ------------> printing ERROR log4j info 1 of DEMO ");
+    }
+
+    @Parameters({"firstName"})
+    @Test(description = "Description in DemoTest", groups = {"Smoke", "Regression"})
+    public void run_demo_test2(String name) {
+        log.info(" Welcome ---  " + name);
     }
 }
